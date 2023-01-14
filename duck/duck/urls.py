cd66dcef21e5f7e086970ad814df9b6254fe4b5a
Krish Shah <krishshah8590@gmail.com>
2023-01-14 13:12:27 -0800

    LET THE DUCKIES QUACK

--- duck/duck/urls.py
+++ duck/duck/urls.py
@@ -1,7 +1,8 @@
-"""website URL Configuration
+"""
+URL configuration for duck project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from duckgame import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index)
 ]
